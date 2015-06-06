7b52395a1b2030204415676c7a71c2972406418c
PolyFloyd <floyd@polyfloyd.net>
2015-06-06 15:45:27 +0200

    Add the pseudorandom program

---
+++ programs/pseudorandom.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+#
+# Copyright (c) 2014 PolyFloyd
+#
+
+import ledbanner
+import math
+import random
+import time
+
+cube          = ledbanner.LEDBanner()
+steps         = [math.sin(i / 40 * math.pi / 2) for i in range(0, 40)]
+frame_current = cube.make_frame()
+frame_target  = cube.make_frame()
+
+while 1:
+	frame_source = frame_target
+	frame_target = cube.make_frame()
+	for i in range(0, len(frame_target)):
+		frame_target[i] = random.randint(0, 255)
+	for m in steps:
+		for j in range(0, len(frame_target)):
+			frame_current[j] = int(frame_source[j] * (1 - m) + frame_target[j] * m)
+		cube.set_frame(frame_current)
+		time.sleep(1 / cube.fps)
