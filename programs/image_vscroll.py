a80c247b2e8019cc711922e9e25ca5ec3aefa732
polyfloyd <floyd@polyfloyd.net>
2017-09-08 20:15:43 +0200

    Make programs compatible with ledcat

--- programs/image_vscroll.py
+++ programs/image_vscroll.py
@@ -1,58 +1,58 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
-#
-# Copyright (c) 2015 Bitlair
-#
-
-# Requires the Pillow library
-
-import ledbanner
+from collections import namedtuple
 import sys
 import time
-from PIL import Image
+from PIL import Image # Requires the Pillow library
+
+DISP_WIDTH  = 150
+DISP_HEIGHT = 16
+
+
+Vector = namedtuple('Vector', 'x y')
 
 if len(sys.argv) == 1:
-	print('Usage: %s <image 1> [<image 2> <image 3> ...]' % sys.argv[0])
+	print('Usage: %s <image 1> [<image 2> <image 3> ...]' % sys.argv[0], file=sys.stderr)
 	exit(1)
 
 def read_image(filename):
 	img = Image.open(filename)
-	img.thumbnail((led.size.x, float('inf')))
+	img.thumbnail((DISP_WIDTH, float('inf')))
 	return img
 
-led    = ledbanner.LEDBanner()
 images = []
-
 for f in sys.argv[1:]:
 	try:
 		image = read_image(f)
-		image_size = ledbanner.Vector(image.size[0], image.size[1])
-		raw = bytearray(image_size.x * image_size.y * led.bytes_per_pixel)
+		image_size = Vector(image.size[0], image.size[1])
+		raw = bytearray(image_size.x * image_size.y * 3)
 		for x in range(0, image_size.x):
 			for y in range(0, image_size.y):
 				pix = image.getpixel((x, y))
 				if type(pix) is int: # Monochrome
 					pix = (pix, pix, pix)
-				i = (y * led.size.x + x) * led.bytes_per_pixel
+				elif len(pix) == 4: # Alhpa
+					pix = pix[:3]
+				i = (y * DISP_WIDTH + x) * 3
 				raw[i:i+3] = pix
 		images.append((raw, image_size))
-	except:
-		pass
+	except Exception as ex:
+		print('Unable to read %s: %s' % (f, ex), file=sys.stderr)
 if len(images) == 0:
-	print('Unable to read any of the specified images')
+	print('Unable to read any of the specified images', file=sys.stderr)
 	exit(1)
 
 while 1:
 	for image, image_size in images:
-		for scroll in range(led.size.y, -image_size.y, -1):
-			frame = led.make_frame()
+		for scroll in range(DISP_HEIGHT, -image_size.y, -1):
+			frame = bytearray(DISP_WIDTH * DISP_HEIGHT * 3)
 
 			frame_start_y = scroll
 			if frame_start_y < 0:
 				frame_start_y = 0
 			frame_stop_y  = scroll + image_size.y
-			if frame_stop_y > led.size.y:
-				frame_stop_y = led.size.y
+			if frame_stop_y > DISP_HEIGHT:
+				frame_stop_y = DISP_HEIGHT
 			image_start_y = -scroll
 			if image_start_y < 0:
 				image_start_y = 0
@@ -60,9 +60,9 @@ while 1:
 			if image_stop_y > image_size.y:
 				image_stop_y = image_size.y
 
-			bytes_per_y = led.size.x * led.bytes_per_pixel
+			bytes_per_y = DISP_WIDTH * 3
 			frame[frame_start_y * bytes_per_y:frame_stop_y * bytes_per_y] = \
 				image[image_start_y*bytes_per_y:image_stop_y*bytes_per_y]
 
-			led.set_frame(frame)
-			time.sleep(1 / led.fps)
+			sys.stdout.buffer.write(frame)
+			time.sleep(1 / 60 * 2)
--- programs/pseudorandom.py
+++ programs/pseudorandom.py
@@ -1,26 +1,25 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3
 
-#
-# Copyright (c) 2014 PolyFloyd
-#
-
-import ledbanner
 import math
 import random
+import sys
 import time
 
-cube          = ledbanner.LEDBanner()
+DISP_WIDTH  = 150
+DISP_HEIGHT = 16
+
+
 steps         = [math.sin(i / 40 * math.pi / 2) for i in range(0, 40)]
-frame_current = cube.make_frame()
-frame_target  = cube.make_frame()
+frame_current = bytearray(DISP_WIDTH * DISP_HEIGHT * 3)
+frame_target  = bytearray(DISP_WIDTH * DISP_HEIGHT * 3)
 
 while 1:
 	frame_source = frame_target
-	frame_target = cube.make_frame()
+	frame_target = bytearray(DISP_WIDTH * DISP_HEIGHT * 3)
 	for i in range(0, len(frame_target)):
 		frame_target[i] = random.randint(0, 255)
 	for m in steps:
 		for j in range(0, len(frame_target)):
 			frame_current[j] = int(frame_source[j] * (1 - m) + frame_target[j] * m)
-		cube.set_frame(frame_current)
-		time.sleep(1 / cube.fps)
+		sys.stdout.buffer.write(frame_current)
+		time.sleep(1 / 60)
